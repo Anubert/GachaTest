8feeae8b10dd977180777096d77df0083fcc187f
Monkeman <persona4269@gmail.com>
2025-07-24 18:58:34 +0200

    Add files via upload

---
+++ traits.js
@@ -0,0 +1,5 @@
+export const traits = [
+  { name: "Moon-Eyed", rarity: 10, description: "Sees into the unseen." },
+  { name: "Iron Lungs", rarity: 6, description: "Endurance beyond human norms." },
+  { name: "Doom’s Whisper", rarity: 0.5, description: "Rare trait that bends fate itself." }
+];
