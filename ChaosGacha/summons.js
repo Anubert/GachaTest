f2a3c7dab4c7c74e453e8e0b9b63c89dd5eb2091
Monkeman <persona4269@gmail.com>
2025-07-26 22:13:00 +0200

    Delete ChaosGacha directory

--- ChaosGacha/summons.js
+++
@@ -1,5 +0,0 @@
-export const summons = [
-  { name: "Jellycat", rarity: 7, description: "Squishy and mildly toxic. Loves hugs." },
-  { name: "Titan Wyrm", rarity: 2, description: "Devours enemies and also trees." },
-  { name: "Glimmerghoul", rarity: 5, description: "Eats nightmares. Slightly sparkly." }
-];
